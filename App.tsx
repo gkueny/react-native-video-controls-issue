f7ae51e3495cdbc26bbf137130e873bb468d027f
gkueny <gkueny.me@gmail.com>
2025-10-30 23:01:48 +0100

    setup issue controle style

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Video from 'react-native-video';
 
@@ -15,15 +15,39 @@ function App() {
 
 const videoUrl =
   'https://test-videos.co.uk/vids/bigbuckbunny/mp4/av1/360/Big_Buck_Bunny_360_10s_1MB.mp4';
+const controlsStyles = {
+  hidePosition: false,
+  hidePlayPause: false,
+  hideForward: false,
+  hideRewind: false,
+  hideNext: false,
+  hidePrevious: false,
+  hideFullscreen: true,
+  hideSeekBar: true,
+  hideDuration: true,
+  hideNavigationBarOnFullScreenMode: true,
+  hideNotificationBarOnFullScreenMode: true,
+  hideSettingButton: true,
+  seekIncrementMS: 10000,
+  liveLabel: 'LIVE',
+};
 function AppContent() {
   return (
-    <Video source={{ uri: videoUrl }} style={styles.video} controls={false} />
+    <View style={styles.container}>
+      <Video
+        source={{ uri: videoUrl }}
+        style={styles.video}
+        controls={true}
+        controlsStyles={controlsStyles}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'black',
   },
   video: {
     flex: 1,
